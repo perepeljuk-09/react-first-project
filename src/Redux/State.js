9be5973d749397cec03ba5e890be7f2fbdbd8514
perepeljuk-09 <77107021+perepeljuk-09@users.noreply.github.com>
2022-06-23 16:43:44 +0300

    add store 37

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,63 +1,72 @@
-import {rerenderEntireTree} from "../render";
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11},
+        {id: 3, message: 'Blabla', likesCount: 31},
+        {id: 4, message: 'fourth post', likesCount: 78}
+      ],
+      newPostText: 'zhivanchi'
+    },
+    messagePage: {
 
-let state = {
-  profilePage: {
-    posts: [
-      {id: 1, message: 'Hi, how are you?', likesCount: 12},
-      {id: 2, message: 'It\'s my first post', likesCount: 11},
-      {id: 3, message: 'Blabla', likesCount: 31},
-      {id: 4, message: 'fourth post', likesCount: 78}
-    ],
-    newPostText: 'zhivanchi'
+      dialogs: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+      ],
+      messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it-kamasutra?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'fourth message'},
+        {id: 5, message: 'fifth message'}
+      ],
+      newMessageText: 'add text'
+    }
   },
-  messagePage: {
+  getState() {
+    return this._state
+  },
+  _callSubscriber () {
 
-    dialogs: [
-      {id: 1, name: 'Dimych'},
-      {id: 2, name: 'Andrew'},
-      {id: 3, name: 'Sveta'},
-      {id: 4, name: 'Sasha'},
-      {id: 5, name: 'Viktor'},
-      {id: 6, name: 'Valera'}
-    ],
-    messages: [
-      {id: 1, message: 'Hi'},
-      {id: 2, message: 'How is your it-kamasutra?'},
-      {id: 3, message: 'Yo'},
-      {id: 4, message: 'fourth message'},
-      {id: 5, message: 'fifth message'}
-    ],
-    newMessageText: 'add text'
+  },
+  addMessage() {
+    let newMessage = {
+      id: 6,
+      message: this._state.messagePage.newMessageText
+    }
+    this._state.messagePage.messages.push(newMessage);
+    this._state.messagePage.newMessageText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewTextMessage(newText) {
+    this._state.messagePage.newMessageText = newText
+    this._callSubscriber(this._state);
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewTextPost(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  Subscribe(observer) {
+    this._callSubscriber = observer;
   }
-}
-window.state = state;
-export let addMessage = () => {
-  let newMessage = {
-    id: 6,
-    message: state.messagePage.newMessageText
-  }
-  state.messagePage.messages.push(newMessage);
-  state.messagePage.newMessageText = '';
-  rerenderEntireTree(state);
-}
-export let updateNewTextMessage = (newText) => {
-  state.messagePage.newMessageText = newText
-  rerenderEntireTree(state);
+
 }
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-}
-export let updateNewTextPost = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-export default state;
+window.store = store;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
-import state from "./Redux/State";
+import store from "./Redux/State";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+
 // AddPost('SamuraiJS.COM')
 
 
-
-  rerenderEntireTree(state);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let rerenderEntireTree = (state) => {
+  root.render(
+      <BrowserRouter>
+        <React.StrictMode>
+          <App state={state}
+               addPost={store.addPost.bind(store)}
+               updateNewTextPost={store.updateNewTextPost.bind(store)}
+               updateNewTextMessage={store.updateNewTextMessage.bind(store)}
+               addMessage={store.addMessage.bind(store)}/>
+        </React.StrictMode>
+      </BrowserRouter>
+  );
+}
+  rerenderEntireTree(store.getState());
+  store.Subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

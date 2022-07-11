6a1217e6c799b92fbf538bf76a0360493ee35057
perepeljuk-09 <77107021+perepeljuk-09@users.noreply.github.com>
2022-07-11 14:11:33 +0300

    61 add auth-Reducer

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -2,6 +2,7 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 
 
@@ -9,7 +10,8 @@ let reducers = combineReducers({
 
   profilePage: profileReducer,
   messagePage: dialogsReducer,
-  usersPage: usersReducer
+  usersPage: usersReducer,
+  auth: authReducer
 });
 
 let store = createStore(reducers);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
         <img src='https://logos-download.com/wp-content/uploads/2021/01/Moscow_Logo_black_text-2048x1364.png' alt="Moscow logotype"/>
+        <div className={s.loginBlock}>
+            { props.isAuth ? <div>{props.login}</div>
+                :<NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 }
 

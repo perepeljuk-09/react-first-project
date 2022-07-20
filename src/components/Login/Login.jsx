d8682ef70ae0761231463c895cc2f01af1923c8b
perepeljuk-09 <77107021+perepeljuk-09@users.noreply.github.com>
2022-07-20 13:16:18 +0300

    76 add login form
    formik

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import {Field, Form, Formik} from "formik";
+import * as yup from 'yup'
 
-const Login = (props) => {
-  return <h1>LOGIN</h1>
+const LoginContainer = (props) => {
+  return <LoginForm/>
 }
 
-export default Login;
+const LoginForm = () => {
+
+  const validationSchema = yup.object().shape({
+    email: yup.string().email('Введите верный email').required('Обязательное поле'),
+    password: yup.string().typeError('Должно быть строкой').required('Обязательное поле')
+  })
+
+  return <div>
+    <h1>Login form</h1>
+    <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+        validateOnBlur
+        validationSchema={validationSchema}
+    >
+      {({
+          values, errors, touched,
+          handleBlur, handleChange, dirty, isValid, handleSubmit
+        }) => (
+          <Form>
+            <label htmlFor={"email"}>Login</label>
+            <p>
+              <Field type="email" name="email" placeholder={"Enter your email"}
+              onChange={handleChange} onBlur={handleBlur} value={values.email}
+              />
+            </p>
+            { touched.email && errors.email && <p>{errors.email}</p>}
+            <label htmlFor={"password"}>Password</label>
+            <p>
+              <Field type="password" name="password" placeholder={"Enter your password"}
+                     onChange={handleChange} onBlur={handleBlur} value={values.password}/>
+            </p>
+            { touched.password && errors.password && <p>{errors.password}</p>}
+            <button type={"submit"} disabled={!isValid && !dirty} onClick={handleBlur}>
+              Submit
+            </button>
+          </Form>
+      )}
+    </Formik>
+  </div>
+}
+
+export default LoginContainer;
